d5369ee0aa7947ffacb6ec11dab5bb5ce194ef2c
silberbullet <pkw961028@naver.com>
2025-05-13 17:49:08 +0900

    feat(main-runner): add rest-client module

--- main-runner/build.gradle.kts
+++ main-runner/build.gradle.kts
@@ -4,7 +4,7 @@ version = "0.0.1-SNAPSHOT"
 
 dependencies {
     // core
-
+    implementation(project(":custom-rest-client"))
     // service
     implementation(project(":school"))
     implementation(project(":student"))
